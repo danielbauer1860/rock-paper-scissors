39df79ce8af2c1b9e63c7880c6a1be99ebca3fb3
danielbauer1860 <danielbauer1860@gmail.com>
2020-10-17 12:32:54 +0200

    Implemented a basic game in the console; the user can enter his choice in a text field; need to fix a bug where the game result is only displayed on a additional click

---
+++ scripts.js
@@ -0,0 +1,80 @@
+const roll = document.getElementById("roll");
+const playerPicker = document.getElementById("playerPicker");
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+roll.onclick = function () {
+    if (playerScore < 5 & computerScore < 5) {
+        game ();
+    }else {
+        console.log(gameOver());
+        reset();
+    }
+}
+
+function game() {
+    playerSelection = playerPicker.value;
+    computerSelection = computerPlay();
+    console.log("Player selected " + playerSelection.toLowerCase());
+    console.log("Computer selected " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log ("Player: " + playerScore + " Computer: "+ computerScore);
+}
+
+function reset() {
+    computerScore = 0;
+    playerScore = 0;
+}
+
+function gameOver() {
+    if (playerScore == 5) {
+        return "Congratulations! You win!"
+    }else {
+        return "You lose!"
+    }
+}
+
+function computerPlay (){
+    let roll = Math.floor(Math.random() * 3) + 1;
+    switch (roll) {
+        case 1 : 
+        return "rock";
+        case 2 : 
+        return "paper";
+        case 3 : 
+        return "scissors";
+    }
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection){
+        return "It's a tie";
+    } 
+    else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "rock") {
+        computerScore++;
+        return "Rock beats Scissors! Computer wins";
+    }
+    else if (playerSelection.toLowerCase() == "rock" & computerSelection == "scissors") {
+        playerScore++;
+        return "Rock beats Scissors! Player wins";
+    }
+    else if (playerSelection.toLowerCase() == "rock" & computerSelection == "paper") {
+        computerScore++;
+        return "Paper beats Rock! Computer wins";
+    }
+    else if (playerSelection.toLowerCase() == "paper" & computerSelection == "rock") {
+        playerScore++;
+        return "Paper beats Rock! Player wins";
+    }
+    else if (playerSelection.toLowerCase() == "paper" & computerSelection == "scissors") {
+        computerScore++;
+        return "Scissors beats Paper! Computer wins";
+    }
+    else if (playerSelection.toLowerCase() == "scissors" & computerSelection == "paper") {
+        playerScore++;
+        return "Scissors beats Paper! Player wins";
+    }
+}
+
